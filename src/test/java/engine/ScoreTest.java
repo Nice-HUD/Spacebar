0fad8917b8ec4b4b39974db6584d91d15d6602e9
kojesung <js95112345@gmail.com>
2024-11-17 15:48:37 +0900

    feat : JaCoCo적용 확인을 위한 테스트코드

--- src/test/java/engine/ScoreTest.java
+++ src/test/java/engine/ScoreTest.java
@@ -1,2 +1,13 @@
-package engine;public class ScoreTest {
+package engine;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreTest {
+    @Test
+    void testScoreCreation() {
+        Score score = new Score("TST", 100);
+        assertEquals("TST", score.getName());
+        assertEquals(100, score.getScore());
+    }
 }
